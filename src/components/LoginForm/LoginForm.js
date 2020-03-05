08c684899855fa7c24c9c1f3f8b226d7175104f0
Ben Harris <benjaminharris28@gmail.com>
2020-03-05 09:51:01 -0500

    updated with protect endpoints and login

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
 import TokenService from '../../services/token-service'
+import AuthApiService from '../../services/auth-api-service'
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -9,18 +10,25 @@ export default class LoginForm extends Component {
 
   state = { error: null }
 
-  handleSubmitBasicAuth = ev => {
+  handleSubmitJwtAuth = ev => {
     ev.preventDefault()
+    this.setState({ error: null })
     const { user_name, password } = ev.target
 
-    TokenService.saveAuthToken(
-      TokenService.makeBasicAuthToken(user_name.value, password.value)
-    )
+    AuthApiService.postLogin({
+      user_name: user_name.value,
+      password: password.value,
+  
+    })
 
-    user_name.value = ''
-    password.value = ''
-    this.props.onLoginSuccess()
+    .then(res => {
+      user_name.value = ''
+      password.value = ''
+      TokenService.saveAuthToken(res.authToken)
+      this.props.onLoginSuccess()
+    })
   }
+
 
   render() {
     const { error } = this.state
